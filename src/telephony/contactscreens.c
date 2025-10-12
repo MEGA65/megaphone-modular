20f80ebd0218731014cdd6bd9bcd1d0b6554b3d2
Paul Gardner-Stephen <paul@m-e-g-a.org>
2025-10-12 11:01:04 +1030

    refactor contact display

--- src/telephony/contactscreens.c
+++ src/telephony/contactscreens.c
@@ -3,6 +3,11 @@
 #include "contactscreens.h"
 
 #include "screen.h"
+#include "records.h"
+
+// label width must be odd to not cause weird GOTOX glitch with CHARY16 mode
+#define LABEL_WIDTH_PX 47
+#define LABEL_WIDTH_GL 10
 
 char contact_draw(uint8_t x, uint8_t y,
 		  uint16_t x_start_px,
@@ -11,31 +16,42 @@ char contact_draw(uint8_t x, uint8_t y,
 		  uint8_t active_field,
 		  unsigned char *contact_record)
 {
+  unsigned char *string;
+
   if (w_gl<20) return 1;
   if (w_px<96) return 2;
 
-  draw_string_nowrap(x, y,
-		     FONT_UI,
-		     0x0f, // light grey for label text
-		     (unsigned char *)"Name:",
-		     x_start_px,
-		     x_start_px + 48,
-		     12,
-		     NULL,
-		     VIEWPORT_PADDED,
-		     NULL,
-		     NULL);
-  draw_string_nowrap(x, y+2,
-		     FONT_UI,
-		     0x0f, // light grey for label text
-		     (unsigned char *)"Number:",
-		     x_start_px,
-		     x_start_px + 48,
-		     12,
-		     NULL,
-		     VIEWPORT_PADDED,
-		     NULL,
-		     NULL);
-  
+  uint8_t fields[3]={FIELD_FIRSTNAME, FIELD_LASTNAME, FIELD_PHONENUMBER};
+  unsigned char *labels[3]
+    ={(unsigned char*)"First:",(unsigned char *)"Last:",(unsigned char *)"Phone:"};
+    
+  for(uint8_t field=0;field<3;field++) {
+    
+    draw_string_nowrap(x, y+field,
+		       FONT_UI,
+		       0x0f, // light grey for label text
+		       labels[field],
+		       x_start_px,
+		       LABEL_WIDTH_PX,
+		       x + LABEL_WIDTH_GL,
+		       NULL,
+		       VIEWPORT_PADDED,
+		       NULL,
+		       NULL);
+
+    string = find_field(contact_record, RECORD_DATA_SIZE, fields[field],NULL);
+    draw_string_nowrap(x + LABEL_WIDTH_GL, y+field,
+		       FONT_UI,
+		       active_field==(field+1) ? 0x8f : 0x8b, // reverse medium grey if not selected
+		       (unsigned char *)string,
+		       x_start_px + LABEL_WIDTH_PX,
+		       w_px - LABEL_WIDTH_PX,
+		       x + w_gl,
+		       NULL,
+		       VIEWPORT_PADDED,
+		       NULL,
+		       NULL);
+  }
+
   return 0;
 }
